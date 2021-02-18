b114437912e3c7e75df7f0ab12d1f80e305672a4
Parul Rani <rathiparul90@gmail.com>
2021-02-18 20:30:19 +0000

    changes to fix issue.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,13 +3,13 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName="";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions=[];
-let correctAnswers=[];
+let question= "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer= "" ;
+let questions=["Who was the first American woman in space? ","True or false: 5000 meters == 5 kilometers? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "]
+let correctAnswers=["Sally Ride","True","40","Trajectory","3"];
 let candidateAnswers=[];
 let numberOfCorrectAnswer=0;
 
@@ -22,36 +22,38 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  correctAnswers.push("Sally Ride");
-  correctAnswers.push("True");
-  correctAnswers.push("40");
-  correctAnswers.push("Trajectory");
-  correctAnswers.push("3");
-  questions.push("Who was the first American woman in space? ");
-  questions.push("True or false: 5000 meters = 5 kilometers. ");
-  questions.push("(5 + 3)/2 * 10 = ? ");
-  questions.push("Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ");
-  questions.push("What is the minimum crew size for the ISS? ")
-  
+/*console.log(question);
+candidateAnswer=input.question("Your Answer: ");*/
+   
   for(let i=0;i<questions.length;i++)
   {
     console.log(`${i+1}) ${questions[i]}`);
     candidateAnswers.push(input.question("Your Answer: "));
-    console.log(`Correct Answer: ${correctAnswers[i]}\n`);
+    //console.log(`Correct Answer: ${correctAnswers[i]}\n`);
 
-  } 
+  }
   }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+
+ /* if(candidateAnswer.toLowerCase()===correctAnswer.toLowerCase())
+  {
+    console.log("correct answer");
+  }*/
   let grade;
+
   
   for(let i=0;i<correctAnswers.length;i++)
   {
     if((correctAnswers[i]).toLowerCase()==(candidateAnswers[i]).toLowerCase())
     {
+      console.log(`Question ${i+1} Correct!!`)
       numberOfCorrectAnswer+=1;
+    }
+    else{
+      console.log(`Question ${i+1} InCorrect! Correct Answer is ${correctAnswers[i]}`)
     }
   }
   grade=(numberOfCorrectAnswer/questions.length)*100;
@@ -63,7 +65,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  //console.log(`Hello ${candidateName} Welcome into the Quiz`);
+  console.log(`Hello ${candidateName} Welcome into the Quiz`);
   
   
   askQuestion();
